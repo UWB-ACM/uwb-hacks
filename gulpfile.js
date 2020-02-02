c99dde08c3fb12b97ad25a96341071ddb339d80f
Travis CI / UWB-ACM <crobberino@gmail.com>
2020-02-02 01:26:43 +0000

    Travis #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var paths = ['./*.scss', 'archive/**/*.scss']
 
 // compile the sass in the current directory
 gulp.task('sass', function(){
-    return gulp.src('*.scss')
+    return gulp.src(paths, {base: './'})
     .pipe(sass())
     .pipe(gulp.dest('.'))
 });
