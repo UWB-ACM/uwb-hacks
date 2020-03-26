6c23c8c05cc08e9b10673fa1e441745f0ca37c35
Lizzy Kate <lizzy.presland@gmail.com>
2020-03-26 16:55:17 -0700

    Update website content for fully virtual event (#31)
    * Move tab navigation scripts into .js file
    
    * Restructure event schedule and timed strikethrough
    
    * Clean up sponsorship mentions
    
    Added external link for M365 div and corrected DigitalOcean spelling
    (there should never be a space between "Digital" and "Ocean").
    
    * Comment out test code for schedule updates
    
    * Update details and phrasing for "About" card
    
    * Update details and phrasing for "Resources" card
    
    * Update details and phrasing for "FAQ" card
    
    * Update details and phrasing for "Sponsors" card
    
    * Add DevPost page link to schedule
    
    * Edit front matter to include Discord & DevPost
    
    * Add Zoom link for team formation mixer
    
    * Push first workshop time to 11AM instead of 10:45
    
    * Add schedule items for Saturday
    
    * Add AFK Photography Contest to schedule / About
    
    * Wrap up changes based on Chris's reviews

---
+++ schedule.js
@@ -0,0 +1,24 @@
+function strikethroughschedule() { //cross out schedule events as day progresses 
+    var today = new Date();
+    // TESTING ONLY:
+    // var today = new Date(2020, 3, 21, 18, 1);
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    
+    if(date < "2020-4-17"){return;} //check for hackathon date, exit if not hackathon
+    
+    var currenttime = today.getHours() + ":" + today.getMinutes()
+    var tables = document.getElementsByClassName("schedule"); 
+
+    for(var i = 0; i < tables.length; i++){ //loop through tables
+        if (tables[i].getAttribute("name") > date) { continue; }
+        for (var j = 0; j < tables[i].rows.length; j++) {
+            //compare current time with hidden column time 
+            if (tables[i].getAttribute("name") < date || ( tables[i].getAttribute("name") == date && tables[i].rows[j].cells[0].innerText < currenttime)) { 
+                //cross out rows before hidden column time
+                tables[i].rows[j].cells[0].innerHTML = "<s>" + tables[i].rows[j].cells[0].innerHTML + "</s>";
+                tables[i].rows[j].cells[1].innerHTML = "<s>" + tables[i].rows[j].cells[1].innerHTML + "</s>";
+            }
+        }
+    }
+
+}
