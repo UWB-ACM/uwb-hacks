56cc81bf51fefa5fd7fc4f725dd8af21167aa9b9
mitchelldang <mlcdang@uw.edu>
2021-02-25 16:59:11 -0800

    add contact page, disable button on submitting

--- scripts.js
+++ scripts.js
@@ -6,20 +6,21 @@
 // navbar slide down when scroll up
 let prevScrollpos = window.pageYOffset;
 window.onscroll = () => {
-let currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
   } else {
     document.getElementById("header").style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
 // ==========================================
 
 // ======= START OF SCRIPTS FOR FOOTER ======
 const year = new Date().getFullYear();
 const cpyright = "Copyright &copy; UWB ACM " + year;
-var footer_content = `
+var footer_content =
+  `
   <footer class="row d-flex justify-content-center">
     <div class="d-inline-block col-sm-4 footer-links">
       <a class="d-inline-block footer-links-text col-sm-12" href="#" title="UWB Hacks logo">Code of Conduct</a>
@@ -35,25 +36,25 @@ var footer_content = `
         <a class="d-inline-block footer-icons" href="https://www.instagram.com/uwb_acm/" target="_blank" title="instagram"><span class="fab fa-instagram" ></span></a>
     </div>
 
-    <div class="d-inline-block col-sm-12 mb-0">` + cpyright + `</div>
+    <div class="d-inline-block col-sm-12 mb-0">` +
+  cpyright +
+  `</div>
 
   </footer>
 `;
 document.getElementById("footer").innerHTML = footer_content;
 
 // ======= START OF SCRIPTS FOR COUNT DOWN ======
-if (obj = document.getElementById("countdown")) {
-
+if ((obj = document.getElementById("countdown"))) {
   const startDate = new Date("Apr 9, 2021 17:30:00").getTime();
   const endDate = new Date("Apr 11, 2021 00:00:00").getTime();
-  var countDownDate = null; 
-  
+  var countDownDate = null;
+
   // Update the count down every 1 second
-  var x = setInterval(function() {
-    
+  var x = setInterval(function () {
     // Get today's date and time
     var now = new Date().getTime();
-   
+
     if (now < startDate) countDownDate = startDate;
     else countDownDate = endDate;
 
@@ -62,23 +63,61 @@ if (obj = document.getElementById("countdown")) {
 
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="countdown"
-    if (now < startDate) document.getElementById("box-title").innerHTML= "starts in";
-    else if (now < endDate) document.getElementById("box-title").innerHTML = "ends in";
+    if (now < startDate)
+      document.getElementById("box-title").innerHTML = "starts in";
+    else if (now < endDate)
+      document.getElementById("box-title").innerHTML = "ends in";
     else document.getElementById("box-title").innerHTML = "Hackathon concluded";
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
       document.getElementById("countdown").innerHTML = "See y'all next year !!";
     }
-  }, 1000);	
+  }, 1000);
 }
 // ==========================================
 
+const scriptURLContact =
+  "https://script.google.com/macros/s/AKfycbwMmad4FvdgEuv1msTDpekCzzAsQYdcL_75SrCoS54NUiUw8lXCucEC/exec";
+const contactForm = document.getElementById("contact-form");
+if (contactForm) {
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("hello");
+    const sendStatus = document.getElementById("send-msg-status");
+    const SendButton = document.getElementById("btn-send-msg");
+    SendButton.disabled = true;
+    SendButton.style.color = "gray";
+    SendButton.style.backgroundColor = "white";
+    SendButton.style.pointerEvents = "none";
+    SendButton.innerHTML = "Sending";
+    fetch(scriptURLContact, { method: "POST", body: new FormData(contactForm) })
+      .then((response) => {
+        document.getElementById("send-msg-status").innerHTML =
+          "&#10003; Message sent successfully! We'll get back to you ASAP";
+
+        SendButton.innerHTML = "Send";
+        SendButton.disabled = false;
+        SendButton.style = "auto";
+        contactForm.reset();
+        setTimeout(function () {
+          sendStatus.innerHTML = "";
+        }, 4000);
+      })
+      .catch((error) => {
+        // console.error('Error!', error.message);
+        document.getElementById("send-msg-status").innerHTML =
+          "<p style='color:red'>Error</p>";
+      });
+  });
+}
