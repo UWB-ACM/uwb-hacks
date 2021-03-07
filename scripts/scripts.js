78dc2244ffefa78acab42d13acb892be8bdec28c
Ryan Russell <Ryan.Russell2000@gmail.com>
2021-03-07 12:06:01 -1000

    Recolored nav and fixed images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,16 +4,16 @@
 
 // ======= START OF SCRIPTS FOR HEADER ======
 // navbar slide down when scroll up
-let prevScrollpos = window.pageYOffset;
-window.onscroll = () => {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = () => {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("header").style.top = "0";
+//   } else {
+//     document.getElementById("header").style.top = "-100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
 // ==========================================
 
 // ======= START OF SCRIPTS FOR FOOTER ======
