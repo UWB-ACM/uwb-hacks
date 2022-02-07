c4c3834be77898e067e128433f401d07fea646d3
George <g9c9@outlook.com>
2022-02-06 16:19:30 -0800

    Setup react project and add countdown

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Countdown from "./components/Countdown";
+function App() {
+  return (
+    <>
+      <h1>UWB Hacks The Cloud</h1>
+      <Countdown />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Countdown.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+function Countdown() {
+  const hDate = new Date(2022, 3, 1, 19);
+  const [now, setNow] = useState(new Date());
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const msPerDay = 1000 * 60 * 60 * 24;
+  const msPerHour = 1000 * 60 * 60;
+  const msPerMinute = 1000 * 60;
+  const msPerSecond = 1000;
+
+  useEffect(() => {
+    let id = setInterval(() => {
+        setNow(new Date());
+      }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  });
+
+  useEffect(() => {
+    let dif = parseInt(hDate - now);
+    setDays(parseInt(dif / msPerDay));
+    dif -= days * msPerDay;
+    setHours(parseInt(dif / msPerHour));
+    dif -= hours * msPerHour;
+    setMinutes(parseInt(dif / msPerMinute));
+    dif -= minutes * msPerMinute;
+    setSeconds(parseInt(dif / msPerSecond));
+  }, [now]);
+
+  return (
+    <h2>
+      {days} Days {hours} Hours {minutes} Minutes {seconds} Seconds
+    </h2>
+  );
+}
+export default Countdown;
